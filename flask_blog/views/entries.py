e88bcd6632595bf70b645d828282babcc271835e
yosukeBaya4 <yousken_n72@msn>
2020-04-04 20:39:39 +0900

    section26 done

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -7,7 +7,8 @@ from flask_blog.models.entries import Entry
 def show_entries():
     if not session.get('logged_in'):
         return redirect(url_for('login'))
-    return render_template('entries/index.html')
+    entries = Entry.query.order_by(Entry.id.desc()).all()
+    return render_template('entries/index.html', entries=entries)
 
 @app.route('/entries/new', methods=['GET'])
 def new_entry():
