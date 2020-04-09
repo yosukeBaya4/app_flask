9b7c168a05e15295e47a14732d00731137d3cfd5
yosukeBaya4 <yousken_n72@msn>
2020-04-09 17:09:38 +0900

    section32 done, implemented module Blueprint

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -8,4 +8,8 @@ app.config.from_object('flask_blog.config')
 
 db = SQLAlchemy(app)
 
-from flask_blog.views import views, entries
+from flask_blog.views.views import view
+app.register_blueprint(view)
+
+from flask_blog.views.entries import entry
+app.register_blueprint(entry, url_prefix='/users')
