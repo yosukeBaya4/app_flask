0c3eeb700efc4787499456f4c42091cc168de82d
yosukeBaya4 <yousken_n72@msn>
2020-04-09 15:20:59 +0900

    section31 done

--- flask_blog/views/entries.py
+++ flask_blog/views/entries.py
@@ -2,51 +2,51 @@ from flask import request, redirect, url_for, render_template, flash, session
 from flask_blog import app
 from flask_blog import db
 from flask_blog.models.entries import Entry
+from flask_blog.views.views import login_required
 
 @app.route('/')
+@login_required
 def show_entries():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entries = Entry.query.order_by(Entry.id.desc()).all()
     return render_template('entries/index.html', entries=entries)
 
-@app.route('/entries/new', methods=['GET'])
-def new_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
-    return render_template('entries/new.html')
 
 @app.route('/entries', methods=['POST'])
+@login_required
 def add_entry():
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry(
-        title=request.form['title'],
-        text=request.form['text']
-    )
+            title=request.form['title'],
+            text=request.form['text']
+            )
     db.session.add(entry)
     db.session.commit()
     flash('新しく記事が作成されました')
     return redirect(url_for('show_entries'))
 
+
+@app.route('/entries/new', methods=['GET'])
+@login_required
+def new_entry():
+    return render_template('entries/new.html')
+
+
 @app.route('/entries/<int:id>', methods=['GET'])
+@login_required
 def show_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     return render_template('entries/show.html', entry=entry)
 
+
 @app.route('/entries/<int:id>/edit', methods=['GET'])
+@login_required
 def edit_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     return render_template('entries/edit.html', entry=entry)
 
+
 @app.route('/entries/<int:id>/update', methods=['POST'])
+@login_required
 def update_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     entry.title = request.form['title']
     entry.text = request.form['text']
@@ -55,10 +55,10 @@ def update_entry(id):
     flash('記事が更新されました')
     return redirect(url_for('show_entries'))
 
+
 @app.route('/entries/<int:id>/delete', methods=['POST'])
+@login_required
 def delete_entry(id):
-    if not session.get('logged_in'):
-        return redirect(url_for('login'))
     entry = Entry.query.get(id)
     db.session.delete(entry)
     db.session.commit()
